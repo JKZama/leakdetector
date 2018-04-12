cdebd376979f74f94f05589f47f529823d7aa10b
Jesse Zamazanuk <33404006+JKZama@users.noreply.github.com>
2018-04-11 21:27:07 -0500

    Added all code needed to iterate through nodearray

--- rtumaster.py
+++ rtumaster.py
@@ -33,38 +33,40 @@ def main():
             data = ''
             nodeArray = [1, 2]
             i = 0
-            if sys.argv[1:]:
-                email = sys.argv[1]
-            else:
-                email = defaultEmail
-            #Connect to the slave
-            master = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
-            master.set_timeout(5.0)
-            master.set_verbose(False)
-            #logger.info("connected")
-            #logger.info(master.execute(1, cst.READ_INPUT_REGISTERS, 0, 3))
-            data = master.execute(1, cst.READ_INPUT_REGISTERS, 0, 3)
-            water = data[0]
-            humidity = data[1]
-            temp = data[2]
-            print("Water: " + str(water) + "\tHumidity: " + str(humidity))
-            if(humidity>80 or water>100):
-                print("Leak Detected!")
-                print("Alert sent via email to "+email)
-                print("Waiting for sensor readings to return to normal...")
-                mailsend.sendLeakAlertEmail(email,1)
-                while(humidity>70 or water>100):
-                    data = master.execute(1, cst.READ_INPUT_REGISTERS, 0, 3)
-                    humidity = data[1]
-                    water = data[2]
-                    sleep(2)
-                    print("..")
-                print("Sensor readings returned to average")
-        except modbus_tk.modbus.ModbusError as exc:
-            logger.error("%s- Code=%d", exc, exc.get_exception_code())
-            try:
-                data = master.execute(1,cst.READ_INPUT_REGISTERS, 0, 3)
+            while i < len(nodeArray):
+                if sys.argv[1:]:
+                    email = sys.argv[1]
+                else:
+                    email = defaultEmail
+                #Connect to the slave
+                master = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+                master.set_timeout(5.0)
+                master.set_verbose(False)
+                #logger.info("connected")
+                #logger.info(master.execute(1, cst.READ_INPUT_REGISTERS, 0, 3))
+                data = master.execute(1, cst.READ_INPUT_REGISTERS, 0, 3)
+                water = data[0]
+                humidity = data[1]
+                temp = data[2]
+                print("Node " + str(i))
+                print("Water: " + str(water) + "\tHumidity: " + str(humidity))
+                if(humidity>80 or water>100):
+                    print("Leak detected at node " + str(i))
+                    print("Alert sent via email to "+email)
+                    print("Waiting for sensor readings to return to normal...")
+                    mailsend.sendLeakAlertEmail(email,1)
+                    while(humidity>70 or water>100):
+                        data = master.execute(i, cst.READ_INPUT_REGISTERS, 0, 3)
+                        humidity = data[1]
+                        water = data[2]
+                        sleep(2)
+                        print("..")
+                    print("Sensor readings returned to average")
             except modbus_tk.modbus.ModbusError as exc:
                 logger.error("%s- Code=%d", exc, exc.get_exception_code())
+                try:
+                    data = master.execute(i,cst.READ_INPUT_REGISTERS, 0, 3)
+                except modbus_tk.modbus.ModbusError as exc:
+                    logger.error("%s- Code=%d", exc, exc.get_exception_code())
 if __name__ == "__main__":
     main()
