f6c3505153b51cac41c85b1ef2ce262b4301f1de
Jesse Zamazanuk <jkzq62@mst.edu>
2018-04-16 02:38:35 +0000

    Multi-node working

--- rtumaster.py
+++ rtumaster.py
@@ -23,17 +23,17 @@ PORT = '/dev/ttyUSB0'
 
 def main():
     defaultEmail = "localizedleakdetector@gmail.com"
-    #logger = modbus_tk.utils.create_logger("console")
+    logger = modbus_tk.utils.create_logger("console")
 
     while True:
-        nodeArray = [1]
-        i = 0
-        sleep(2)
+        nodeArray = [1,2]
+        i = 1
         humidity = 1
         water = 1
         temp = 1
         data = ''
-        while i < len(nodeArray):
+        while i < len(nodeArray)+1:
+            sleep(2)
             if sys.argv[1:]:
                 email = sys.argv[1]
             else:
@@ -42,22 +42,22 @@ def main():
             try:
                 master = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
                 master.set_timeout(5.0)
-                master.set_verbose(False)
+                master.set_verbose(True)
                 #logger.info("connected")
                 #logger.info(master.execute(1, cst.READ_INPUT_REGISTERS, 0, 3))
-                data = master.execute(1, cst.READ_INPUT_REGISTERS, 0, 3)
+                data = master.execute(nodeArray[i-1], cst.READ_INPUT_REGISTERS, 0, 3)
                 water = data[0]
                 humidity = data[1]
                 temp = data[2]
-                print("Node " + str(i))
+                print("Node " + str(nodeArray[i-1]))
                 print("Water: " + str(water) + "\tHumidity: " + str(humidity))
                 if(humidity>80 or water>100):
                     print("Leak detected at node " + str(i))
                     print("Alert sent via email to "+email)
                     print("Waiting for sensor readings to return to normal...")
-                    mailsend.sendLeakAlertEmail(email,1)
+                    mailsend.sendLeakAlertEmail(email,nodeArray[i-1])
                     while(humidity>70 or water>100):
-                        data = master.execute(i, cst.READ_INPUT_REGISTERS, 0, 3)
+                        data = master.execute(nodeArray[i-1], cst.READ_INPUT_REGISTERS, 0, 3)
                         humidity = data[1]
                         water = data[2]
                         sleep(2)
@@ -66,7 +66,7 @@ def main():
             except modbus_tk.modbus.ModbusError as exc:
                 logger.error("%s- Code=%d", exc, exc.get_exception_code())
                 try:
-                    data = master.execute(i,cst.READ_INPUT_REGISTERS, 0, 3)
+                    data = master.execute(nodeArray[i-1],cst.READ_INPUT_REGISTERS, 0, 3)
                 except modbus_tk.modbus.ModbusError as exc:
                     logger.error("%s- Code=%d", exc, exc.get_exception_code())
             i += 1
