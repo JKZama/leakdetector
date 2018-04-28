a5db5976624b1384aea16d1029af081c009550c8
Jesse Zamazanuk <33404006+JKZama@users.noreply.github.com>
2018-04-27 19:56:43 -0500

    Update rtumaster.py

--- rtumaster.py
+++ rtumaster.py
@@ -51,9 +51,9 @@ def main():
                 temp = data[2]
                 print("Node " + str(nodeArray[i-1]))
                 print("Water: " + str(water) + "\tHumidity: " + str(humidity))
-                mqttc = mqtt.Client(“python_pub”)
-                mqttc.connect(“test.mosquitto.org”, 1883)
-                mqttc.publish(“hello/world”, “Hello, World!”)
+                mqttc = mqtt.Client("python_pub")
+                mqttc.connect("test.mosquitto.org", 1883)
+                mqttc.publish("hello", "Hello!")
                 mqttc.loop(2)
                 if(humidity>80 or water>100):
                     print("Leak detected at node " + str(i))
